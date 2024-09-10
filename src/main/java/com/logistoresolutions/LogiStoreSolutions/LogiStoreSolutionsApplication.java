418abec295f1568cbe78c0b1304a59f0da2148c8
Paco Molina <pacomolinac2003@gmail.com>
2024-09-10 17:07:28 +0200

    First commit - Spring Boot Application

---
+++ src/main/java/com/logistoresolutions/LogiStoreSolutions/LogiStoreSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.logistoresolutions.LogiStoreSolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiStoreSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiStoreSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistoresolutions/LogiStoreSolutions/LogiStoreSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistoresolutions.LogiStoreSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiStoreSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
